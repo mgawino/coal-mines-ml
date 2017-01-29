e5d22cc2689f6bd7ffcd89b6e26dec0ac89a3890
Mateusz Gawino <mateusz.gawino@gmail.com>
2017-01-29 22:12:16 +0100

    Fixes

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from collections import Counter
 import numpy as np
 
 import click
+from skfeature.function.information_theoretical_based.MRMR import mrmr
 from skfeature.function.statistical_based.gini_index import gini_index
-from skfeature.function.statistical_based.CFS import cfs
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectFromModel
 from sklearn.random_projection import GaussianRandomProjection, SparseRandomProjection
@@ -16,6 +16,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_selection import SelectKBest, f_classif, mutual_info_classif
 from sklearn.metrics import roc_auc_score
 from sklearn.pipeline import Pipeline
+from wrappers import SelectKBestWrapper, cfs_wrapper
 
 
 def create_pipeline(selection_transformer):
@@ -28,20 +29,21 @@ def create_pipeline(selection_transformer):
 def make_selection_transformers():
     random_forest_clf = RandomForestClassifier(n_estimators=100)
     return [
-        SelectKBest(f_classif, k='all'),
-        SelectKBest(mutual_info_classif, k='all'),
-        SelectKBest(gini_index, k='all'),
-        # MRMR
+        # SelectKBest(f_classif, k=5),
+        # SelectKBest(mutual_info_classif, k=5),
+        SelectKBest(gini_index, k=5),
+        # SelectKBestWrapper(cfs_wrapper, k=5),
+        # SelectKBestWrapper(mrmr, k=5),
         # Genetic algorithms
-        SelectFromModel(random_forest_clf, threshold=0.5),
-        PCA(n_components=1),
-        GaussianRandomProjection(n_components=1),
-        SparseRandomProjection(n_components=1)
+        # SelectFromModel(random_forest_clf, threshold=0.5),
+        # PCA(n_components=1),
+        # GaussianRandomProjection(n_components=1),
+        # SparseRandomProjection(n_components=1)
     ]
 
 
-def class_to_binary(iterable):
-    return list(map(lambda x: 1 if x == 'warning' else 0, iterable))
+def class_to_binary(x):
+    return 1 if x == 'warning' else 0
 
 
 def load_data(clear_cache, n_jobs):
@@ -53,42 +55,24 @@ def load_data(clear_cache, n_jobs):
     y_test = DataReader.read_test_labels()
     assert train_features.shape[0] == y_train.shape[0]
     assert test_features.shape[0] == y_test.shape[0]
-    return train_features, class_to_binary(y_train), test_features, class_to_binary(y_test), feature_names
-
-
-def generate_data():
-    features = np.array([
-        [0],
-        [0],
-        [0],
-        [1],
-        [1],
-        [1],
-    ])
-    y = np.array([
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-    ])
-    names = ['a', 'b', 'c', 'd', 'e']
-    return features, y, features, y, names
+    class_to_binary_vec = np.vectorize(class_to_binary)
+    y_train = class_to_binary_vec(y_train)
+    y_test = class_to_binary_vec(y_test)
+    return train_features, y_train, test_features, y_test, feature_names
 
 
 @click.command()
 @click.option('--clear-cache', '-cc', is_flag=True, help='Clear features cache')
 @click.option('--n-jobs', '-j', type=click.INT, help='Feature extraction jobs', default='3')
 def main(clear_cache, n_jobs):
-    train_features, y_train, test_features, y_test, feature_names = generate_data() # FIXME load_data(clear_cache, n_jobs)
+    train_features, y_train, test_features, y_test, feature_names = load_data(clear_cache, n_jobs)
     selection_transformers = make_selection_transformers()
     for label_ix in range(3):
         y_train_label = y_train[:, label_ix]
         y_test_label = y_test[:, label_ix]
-        print('Train labels: {} Test labels: {}'.format(Counter(y_train_label), Counter(y_test_label)))
+        print('Label: {} Train: {} Test: {}'.format(label_ix, Counter(y_train_label), Counter(y_test_label)))
     for selection_transformer in selection_transformers:
-        for label_ix in range(1):  # FIXME
+        for label_ix in range(3):
             y_train_label = y_train[:, label_ix]
             y_test_label = y_test[:, label_ix]
             pipeline = create_pipeline(selection_transformer)
@@ -96,7 +80,7 @@ def main(clear_cache, n_jobs):
             # selected_features = feature_names[selection_transformer.get_support(indices=True)]
             predictions = pipeline.predict(test_features)
             auc_score = roc_auc_score(y_test_label, predictions)
-            print('Selection method: {} AUC: {}'.format(selection_transformer, auc_score))
+            print('Selection method: {} for label: {} AUC: {}'.format(selection_transformer, label_ix, auc_score))
 
 
 if __name__ == '__main__':
