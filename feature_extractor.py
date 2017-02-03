a1e3b1941e7415a9b1161374c72ccae02355127d
Mateusz Gawino <mateusz.gawino@gmail.com>
2017-02-03 20:30:48 +0100

    Final version of feature extractor

--- feature_extractor.py
+++ feature_extractor.py
@@ -2,11 +2,23 @@
 import os
 
 import click
-import numpy as np
-from sklearn.pipeline import Pipeline, FeatureUnion
-from transformer import SensorsTransformer
-from tsfresh.feature_extraction import feature_calculators
+from sklearn.pipeline import FeatureUnion
+from transformer import SensorTransformer, SensorMultiTransformer
+from tsfresh.feature_extraction.feature_calculators import *
 from reader import DataReader
+
+
+class CustomFeatureUnion(FeatureUnion):
+
+    def get_feature_names(self):
+        feature_names = []
+        for name, trans, weight in self._iter():
+            if not hasattr(trans, 'get_feature_names'):
+                raise AttributeError("Transformer %s (type %s) does not "
+                                     "provide get_feature_names."
+                                     % (str(name), type(trans).__name__))
+            feature_names.extend(trans.get_feature_names())
+        return feature_names
 
 
 class FeatureExtractor:
@@ -15,60 +27,95 @@ class FeatureExtractor:
     FEATURE_NAMES_CACHE_PATH = os.path.expanduser('~/feature_names_cache.npy')
 
     def __init__(self, n_jobs):
-        self.transformer = Pipeline([
-            ('features', FeatureUnion([
-                # ('number_peaks_5', SensorsDataTransformer(feature_calculators.number_peaks, n=5)),  # 640
-                # ('number_peaks_10', SensorsDataTransformer(feature_calculators.number_peaks, n=10)),  # 870
-                # ('number_peaks_20', SensorsDataTransformer(feature_calculators.number_peaks, n=20)),  # 1200
-                ('count_above_mean', SensorsTransformer(feature_calculators.count_above_mean)),  # 500
-                ('count_below_mean', SensorsTransformer(feature_calculators.count_below_mean)),  # 500
-                ('autocorrelation_10', SensorsTransformer(feature_calculators.autocorrelation, lag=10)),  # 200
-                ('autocorrelation_20', SensorsTransformer(feature_calculators.autocorrelation, lag=20)),  # 200
-                ('autocorrelation_50', SensorsTransformer(feature_calculators.autocorrelation, lag=50)),  # 200
-                ('max', SensorsTransformer(max)),  # 200
-                ('min', SensorsTransformer(min)),  # 200
-                ('mean_autocorrelation', SensorsTransformer(feature_calculators.mean_autocorrelation)),  # 117
-                ('quantile_0.2', SensorsTransformer(feature_calculators.quantile, q=0.2)),  # 130
-                ('quantile_0.4', SensorsTransformer(feature_calculators.quantile, q=0.4)),  # 130
-                ('quantile_0.6', SensorsTransformer(feature_calculators.quantile, q=0.6)),  # 130
-                ('quantile_0.8', SensorsTransformer(feature_calculators.quantile, q=0.8)),  # 130
-                ('skewness', SensorsTransformer(feature_calculators.skewness)),  # 113
-                ('kurtosis', SensorsTransformer(feature_calculators.kurtosis)),  # 120
-                ('longest_strike_below_mean', SensorsTransformer(feature_calculators.longest_strike_below_mean)),
-                ('longest_strike_above_mean', SensorsTransformer(feature_calculators.longest_strike_above_mean)),
-                ('last_location_of_maximum', SensorsTransformer(feature_calculators.last_location_of_maximum)),
-                ('first_location_of_maximum', SensorsTransformer(feature_calculators.first_location_of_maximum)),
-                ('last_location_of_minimum', SensorsTransformer(feature_calculators.last_location_of_minimum)),
-                ('first_location_of_minimum', SensorsTransformer(feature_calculators.first_location_of_minimum)),
-                ('binned_entropy_5', SensorsTransformer(feature_calculators.binned_entropy, max_bins=5)),
-                ('binned_entropy_10', SensorsTransformer(feature_calculators.binned_entropy, max_bins=10)),
-                ('binned_entropy_20', SensorsTransformer(feature_calculators.binned_entropy, max_bins=20)),
-                ('mean_second_derivate_central', SensorsTransformer(feature_calculators.mean_second_derivate_central)),
-                ('mean', SensorsTransformer(feature_calculators.mean)),
-                ('median', SensorsTransformer(feature_calculators.median)),
-                ('variance', SensorsTransformer(feature_calculators.variance)),
-                ('std', SensorsTransformer(feature_calculators.standard_deviation)),
-                ('large_std', SensorsTransformer(feature_calculators.large_standard_deviation, r=0.5)),
-                ('var_larger_than_std', SensorsTransformer(feature_calculators.variance_larger_than_standard_deviation)),
-                ('sum_values', SensorsTransformer(feature_calculators.sum_values)),
-                ('mean_change', SensorsTransformer(feature_calculators.mean_change)),
-                ('mean_abs_change', SensorsTransformer(feature_calculators.mean_abs_change)),
-                ('absolute_sum_of_changes', SensorsTransformer(feature_calculators.absolute_sum_of_changes)),
-                ('abs_energy', SensorsTransformer(feature_calculators.abs_energy)),
-                # ('fft_coefficient', SensorsDataTransformer(feature_calculators.fft_coefficient)),
-                # ('index_mass_quantile', SensorsDataTransformer(feature_calculators.index_mass_quantile)),
-                # ('number_cwt_peaks', SensorsDataTransformer(feature_calculators.number_cwt_peaks)),
-                # ('cwt_coefficients', SensorsDataTransformer(feature_calculators.cwt_coefficients)),
-                # ('ar_coefficient', SensorsDataTransformer(feature_calculators.ar_coefficient)),
-                # ('time_reversal_asymmetry_statistic', SensorsDataTransformer(feature_calculators.time_reversal_asymmetry_statistic)),
-                ], n_jobs=n_jobs)
+        feature_transformers = [
+            ('max', SensorTransformer(max)),
+            ('min', SensorTransformer(min)),
+            ('mean_autocorrelation', SensorTransformer(mean_autocorrelation)),
+            ('skewness', SensorTransformer(skewness)),
+            ('kurtosis', SensorTransformer(kurtosis)),
+            ('longest_strike_below_mean', SensorTransformer(longest_strike_below_mean)),
+            ('longest_strike_above_mean', SensorTransformer(longest_strike_above_mean)),
+            ('last_location_of_maximum', SensorTransformer(last_location_of_maximum)),
+            ('first_location_of_maximum', SensorTransformer(first_location_of_maximum)),
+            ('last_location_of_minimum', SensorTransformer(last_location_of_minimum)),
+            ('first_location_of_minimum', SensorTransformer(first_location_of_minimum)),
+            ('binned_entropy_10', SensorTransformer(binned_entropy, max_bins=10)),
+            ('mean_second_derivate_central', SensorTransformer(mean_second_derivate_central)),
+            ('mean', SensorTransformer(mean)),
+            ('median', SensorTransformer(median)),
+            ('variance', SensorTransformer(variance)),
+            ('std', SensorTransformer(standard_deviation)),
+            ('var_larger_than_std', SensorTransformer(variance_larger_than_standard_deviation)),
+            ('sum_values', SensorTransformer(sum_values)),
+            ('mean_change', SensorTransformer(mean_change)),
+            ('mean_abs_change', SensorTransformer(mean_abs_change)),
+            ('absolute_sum_of_changes', SensorTransformer(absolute_sum_of_changes)),
+            ('abs_energy', SensorTransformer(abs_energy)),
+        ]
+        for n in [1, 3, 5]:
+            feature_transformers.append(('number_peaks_{}'.format(n), SensorTransformer(number_peaks, n=n)))
+            feature_transformers.append(('large_num_of_peaks_{}'.format(n), SensorTransformer(large_number_of_peaks, n=n)))
+            feature_transformers.append(('cwt_peaks_{}'.format(n), SensorTransformer(number_cwt_peaks, n=n)))
+
+        for lag in range(1, 4):
+            transformer = SensorTransformer(time_reversal_asymmetry_statistic, lag=lag)
+            feature_transformers.append(('time_reversal_{}'.format(lag), transformer))
+
+        for i in range(20):
+            r = 0.05 * i
+            feature_transformers.append(('symmetry_looking_{}'.format(r), SensorTransformer(symmetry_looking, r=r)))
+
+        for i in range(10):
+            r = 0.05 * i
+            feature_transformers.append(('large_std_{}'.format(r), SensorTransformer(large_standard_deviation, r=r)))
+            feature_transformers.append(('autocorrelation_{}'.format(i), SensorTransformer(autocorrelation, lag=i)))
+
+        quantiles = [.1, .2, .3, .4, .6, .7, .8, .9]
+        for q in quantiles:
+            feature_transformers.append(('quantile_{}'.format(q), SensorTransformer(quantile, q=q)))
+
+        feature_transformers.append(
+            ('index_mass_quantile', SensorMultiTransformer(index_mass_quantile, param=[{'q': q} for q in quantiles]))
+        )
+
+        for ql, qh in itertools.product([0., .2, .4, .6, .8], [.2, .4, .6, .8, 1.]):
+            feature_transformers.append(
+                ('mean_abs_change_quantile_{}_{}'.format(ql, qh),
+                 SensorTransformer(mean_abs_change_quantiles, ql=ql, qh=qh))
             )
-        ])
+
+        for r in [.1, .3, .5, .7, .9]:
+            feature_transformers.append(
+                ('approximate_entropy_{}'.format(r), SensorTransformer(approximate_entropy, m=2, r=r))
+            )
+
+        feature_transformers.append(
+            ('ar_coefficient',
+             SensorMultiTransformer(ar_coefficient, param=[{'coeff': coeff, 'k': 10} for coeff in range(5)]))
+        )
+
+        feature_transformers.append(
+            ('cwt_coeff',
+             SensorMultiTransformer(
+                 cwt_coefficients,
+                 param=[{'coeff': coeff, 'widths': (2, 5, 10, 20), 'w': w} for coeff in range(15) for w in (2, 5, 10, 20)]
+             ))
+        )
+
+        feature_transformers.append(
+            ('spkt_welch_density',
+             SensorMultiTransformer(spkt_welch_density, param=[{'coeff': coeff} for coeff in [2, 5, 8]]))
+        )
+
+        feature_transformers.append(
+            ('fft_coefficent',
+             SensorMultiTransformer(fft_coefficient, param=[{'coeff': coeff} for coeff in range(10)]))
+        )
+
+        self.transformer = CustomFeatureUnion(feature_transformers)
         sensor_names = DataReader.get_sensor_names()
-        self.transformer_names, _ = zip(*self.transformer.named_steps['features'].transformer_list)
-        self.feature_names = np.array(['{}_{}'.format(transformer_name, sensor_name)
-                                       for transformer_name in self.transformer_names
-                                       for sensor_name in sensor_names])
+        for _, trans in self.transformer.transformer_list:
+            trans.set_sensor_names(sensor_names)
 
     @classmethod
     def clear_cache(cls):
@@ -103,10 +150,11 @@ class FeatureExtractor:
             X_train_partials.append(X_train_partial)
         X_train = np.concatenate(X_train_partials, axis=0)
         train_features = self.transformer.transform(X_train)
-        assert train_features.shape[1] == len(self.transformer_names) * DataReader.SENSOR_NUM
+        feature_names = np.asarray(self.transformer.get_feature_names())
+        assert train_features.shape[1] == len(feature_names)
         X_test = DataReader.read_test_data()
         test_features = self.transformer.transform(X_test)
-        return train_features, test_features
+        return train_features, test_features, feature_names
 
     def load_features(self):
         if self._cache_exists():
@@ -114,8 +162,7 @@ class FeatureExtractor:
             train_features, test_features, feature_names = self._load_from_cache()
         else:
             click.secho('Transforming data to features', fg='blue')
-            train_features, test_features = self._transform_data_to_features()
-            feature_names = self.feature_names
+            train_features, test_features, feature_names = self._transform_data_to_features()
             self._cache_arrays(train_features, test_features, feature_names)
 
         assert train_features.shape[1] == test_features.shape[1] == len(feature_names)
--- transformer.py
+++ transformer.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
+import pandas as pd
+
 import numpy as np
 
 from sklearn.base import TransformerMixin
 from utils import measure_time
 
 
-class SensorsTransformer(TransformerMixin):
+class SensorTransformer(TransformerMixin):
 
     def __init__(self, function, **function_kwargs):
         self.function = function
         self.function_kwargs = function_kwargs
+        self.sensor_names = None
+
+    def set_sensor_names(self, sensor_names):
+        self.sensor_names = sensor_names
+
+    def get_feature_names(self):
+        fun_name = self.function.__name__
+        fun_kwargs_sufix = '_'.join('{}_{}'.format(k,v) for k, v in self.function_kwargs.items())
+        feature_names = ['{}_{}'.format(sensor_name, fun_name)
+                         for sensor_name in self.sensor_names]
+        if fun_kwargs_sufix:
+            feature_names = ['{}_{}'.format(name, fun_kwargs_sufix) for name in feature_names]
+        return feature_names
 
     @measure_time()
     def transform(self, X):
@@ -19,4 +34,27 @@ class SensorsTransformer(TransformerMixin):
             for sensor_data in row:
                 features.append(self.function(sensor_data, **self.function_kwargs))
             result.append(np.asarray(features, dtype=np.float32))
+        return np.asarray(result, dtype=np.float32)
+
+
+class SensorMultiTransformer(SensorTransformer):
+
+    def __init__(self, function, **function_kwargs):
+        super(SensorMultiTransformer, self).__init__(function, **function_kwargs)
+        self.feature_names = None
+
+    def get_feature_names(self):
+        return self.feature_names
+
+    @measure_time()
+    def transform(self, X):
+        result = []
+        for row in X:
+            features = pd.Series()
+            for sensor_name, sensor_data in zip(self.sensor_names, row):
+                res = self.function(sensor_data, c=sensor_name, **self.function_kwargs)
+                features.append(res)
+            if self.feature_names is None:
+                self.feature_names = list(features.index)
+            result.append(features.values)
         return np.asarray(result, dtype=np.float32)
